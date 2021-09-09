7e3d2f9cb4866c304f3839242d90f3f5e54bc27d
Pasa Yazici <pasa.yazici@morhipo.com>
2021-09-09 10:48:11 +0300

    publish added

---
+++ UI/runpi.sh
@@ -0,0 +1,14 @@
+﻿# https://www.raspberrypi.org/documentation/computers/using_linux.html#running-a-task-on-reboot
+
+# Aşağıdaki komutla bilgisayar her açıldığında ilgili sh dosyasını çalıştırır.
+# Bu bir reboot cronjob 'dır
+# Console da pathini belirt.
+# En sondaki & işareti uygulamanın arka planda çalışacağını belirtir.
+
+# @reboot /home/pi/DotNetCoreApps/MarsLabs/UI/runpi.sh &
+
+# ----- SCRIPT BEGIN ------- #
+
+
+
+# ----- SCRIPT END ------- #
